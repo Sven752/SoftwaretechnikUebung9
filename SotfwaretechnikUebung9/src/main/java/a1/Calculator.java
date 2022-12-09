436bab4fda4ab5ade98d45d5a818007da43d081d
Sven Bergrath <Sven Bergrath@Laptop-Sven-B.fritz.box>
2022-12-09 15:31:05 +0100

    HW9.1

--- SotfwaretechnikUebung9/src/main/java/a1/Calculator.java
+++ SotfwaretechnikUebung9/src/main/java/a1/Calculator.java
@@ -3,19 +3,19 @@ package a1;
 
 public class Calculator {
 
-		public int add(int valueOne, int valueTwo) {
+		public static int add(int valueOne, int valueTwo) {
 			return valueOne + valueTwo;
 		}
 
-		public int sub(int valueOne, int valueTwo) {
+		public static int sub(int valueOne, int valueTwo) {
 			return valueOne - valueTwo;
 		}
 
-		public int multiply(int valueOne, int valueTwo) {
+		public static int multiply(int valueOne, int valueTwo) {
 			return valueOne * valueTwo;
 		}
 
-		public float divide(int numerator, int denominator) {
+		public static float divide(int numerator, int denominator) {
 			if (denominator == 0)
 				throw new ArithmeticException("ZeroDivisionError");
 
---
+++ SotfwaretechnikUebung9/src/test/java/a1/TestCalculator.java
@@ -0,0 +1,43 @@
+package a1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCalculator {
+
+	@Test
+	public void Should_add_two_numbers_and_retrun_the_resulst()
+	{
+		assertEquals(2+3, Calculator.add(2,3));
+	}
+	
+	@Test
+	public void Should_subtract_two_numbers_and_retrun_the_resulst()
+	{
+		assertEquals(4-3, Calculator.sub(4,3));
+	}
+
+	@Test
+	public void Should_multiply_two_numbers_and_retrun_the_resulst()
+	{
+		assertEquals(2*3, Calculator.multiply(2,3));
+	}
+
+	@Test
+	public void Should_divide_two_numbers_and_retrun_the_resulst()
+	{
+		assertEquals(2/3, Calculator.divide(2,3));
+	}
+	
+	@Test
+	public void Should_throw_an_arithmetic_exeption_if_denominator_is_zero()
+	{
+		Exception e = new ArithmeticException("ZeroDivisionError");
+		assertEquals(e, Calculator.divide(2,0));
+
+	}
+
+
+
+}
