98e4a0ef71cfac96c95d911e1c067b1ffd9cffc4
Sven Bergrath <Sven Bergrath@Laptop-Sven-B.fritz.box>
2022-12-10 16:17:17 +0100

    HW9.4

--- SotfwaretechnikUebung9/src/main/java/a3/FizzBuzz.java
+++ SotfwaretechnikUebung9/src/main/java/a3/FizzBuzz.java
@@ -2,4 +2,20 @@ package a3;
 
 public class FizzBuzz {
 
+	public static Object convertNumber(int number) {
+		if(number%5 == 0 && number%7 != 0)
+		{
+			return "Fizz";
+		}
+		if(number%5 != 0 && number%7 == 0)
+		{
+			return "Buzz";
+		}
+		if(number%5 == 0 && number%7 == 0)
+		{
+			return "FizzBuzz";
+		}
+		return null;
+	}
+
 }
